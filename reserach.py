246b46c7d7b776fabd271cbbb10fe6a367f8d8d3
Superior-Snacks <joningiwork@gmail.com>
2025-09-30 17:36:29 +0000

    s

--- reserach.py
+++ reserach.py
@@ -119,6 +119,7 @@ def basic_buy_no_algo(trades):
     """
     for tr in trades:
         if calc_dollar_value(tr["price"],tr["size"]) > 80:
+            print(f"Bought at {tr["price"]}")
 
 def main():
     offset_trade = 4811 + 49999
