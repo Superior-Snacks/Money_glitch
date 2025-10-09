2216d247b411a596c9fccd857f377f4e29789971
Superior-Snacks <joningiwork@gmail.com>
2025-10-09 10:48:51 +0000

    s

--- reserach.py
+++ reserach.py
@@ -330,7 +330,8 @@ def main():
 
             # 3) (Optional) print live lock stats
             if settled:
-                print(f"SETTLED ${settled["proceeds"]:.2f} | {settled["entry_time"]} || {settled["settle_time"]}")
+                for i in settled:
+                    print(f"SETTLED ${i["proceeds"]:.2f} | {i["entry_time"]} || {i["settle_time"]}")
             print(f"LOCKED NOW: ${locked_now:.2f} | PEAK LOCKED: ${peak_locked:.2f} at {peak_locked_time}")
 
 
