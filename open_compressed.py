d3c3ea9a177a49d4dc163bd059de9e622d6480bc
Superior-Snacks <joningiwork@gmail.com>
2025-10-12 14:58:16 +0000

    s

--- open_compressed.py
+++ open_compressed.py
@@ -1,4 +1,5 @@
 import gzip, json
+import pandas as pd
 
 path = "logs/trades_taken_2025-10-11.jsonl.gz"  # your file
 
@@ -6,3 +7,6 @@ with gzip.open(path, "rt", encoding="utf-8") as f:
     for line in f:
         record = json.loads(line)
         print(record)
+
+df = pd.read_json(path, lines=True, compression="gzip")
+print(df.head())
